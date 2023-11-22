e9cdb046454739962a0dc59fddd70a9924e7f628
bahylol <94308278+bahylol@users.noreply.github.com>
2023-11-22 16:27:48 +0200

    Created logging microservice file structure and database models

---
+++ logging/model/logging.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const logSchema = new Schema(
+  {
+    time: {
+      type: Date,
+      default: Date.now,
+    },
+
+    status: {
+      type: String,
+      required: true,
+    },
+
+    method: {
+      type: String,
+      required: true,
+    },
+
+    api: {
+      type: String,
+      required: true,
+    },
+
+    details: {
+      type: String,
+    },
+
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: 'users',
+    },
+  },
+  {
+    strict: true,
+  }
+);
+
+const Log = mongoose.model("log", logSchema);
+
+module.exports = Log;
