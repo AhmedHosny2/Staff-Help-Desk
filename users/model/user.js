064da3fd85532499719b8c448733dd9e7ee67122
Ahmed Yehia <Ahmed.Hosny4434@gmail.com>
2023-11-26 11:01:34 +0200

    Adding utilization level for agents

--- users/model/user.js
+++ users/model/user.js
@@ -1,95 +1,109 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-	firstName: {
-		type: String,
-		required: true,
-	},
+  firstName: {
+    type: String,
+    required: true,
+  },
 
-	lastName: {
-		type: String,
-		required: true,
-	},
+  lastName: {
+    type: String,
+    required: true,
+  },
 
-	phoneNumber: String,
+  phoneNumber: String,
 
-	address: String,
+  address: String,
 
-	role: {
-		type: String,
-		enum: ['user', 'admin', 'manager', 'agent1', 'agent2', 'agent3'],
-		required: true,
-	},
+  role: {
+    type: String,
+    enum: ["user", "admin", "manager", "agent1", "agent2", "agent3"],
+    required: true,
+  },
+  utilization: {
+    Software: {
+      type: Number,
+      required: false,
+    },
+    Hardware: {
+      type: Number,
+      required: false,
+    },
+    Network: {
+      type: Number,
+      required: false,
+    },
+  },
 
-	email: {
-		type: String,
-		required: true,
-		unique: true,
-	},
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
 
-	status: {
-		type: String,
-		required: false,
-	},
+  status: {
+    type: String,
+    required: false,
+  },
 
-	hash: {
-		type: String,
-		required: true,
-	},
+  hash: {
+    type: String,
+    required: true,
+  },
 
-	salt: {
-		type: String,
-		required: true,
-	},
+  salt: {
+    type: String,
+    required: true,
+  },
 
-	// PIN for Multi-Factor Authentication (MFA)
-	pin: {
-		type: String,
-	},
-	tempPin: {
-		type: String,
-	},
+  // PIN for Multi-Factor Authentication (MFA)
+  pin: {
+    type: String,
+  },
+  tempPin: {
+    type: String,
+  },
 });
 
 const brandInfoSchema = new Schema(
-	{
-		color: {
-			type: String,
-			required: true,
-		},
+  {
+    color: {
+      type: String,
+      required: true,
+    },
 
-		logo: {
-			type: String,
-			required: true,
-		},
+    logo: {
+      type: String,
+      required: true,
+    },
 
-		slogan: {
-			type: String,
-			required: true,
-		},
+    slogan: {
+      type: String,
+      required: true,
+    },
 
-		name: {
-			type: String,
-			required: true,
-		},
+    name: {
+      type: String,
+      required: true,
+    },
 
-		font: {
-			type: String,
-			required: true,
-		},
-	},
-	{
-		strict: true,
-	}
+    font: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    strict: true,
+  }
 );
 
 // // Define the models
-const userModel = mongoose.model('user', userSchema);
-const brandInfoModel = mongoose.model('brandInfo', brandInfoSchema);
+const userModel = mongoose.model("user", userSchema);
+const brandInfoModel = mongoose.model("brandInfo", brandInfoSchema);
 
 // // Export the models
 module.exports = {
-	userModel,
-	brandInfoModel,
+  userModel,
+  brandInfoModel,
 };
