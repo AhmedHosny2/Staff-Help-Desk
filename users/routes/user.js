60dd37ab25248c75a8e1684739c1af1982f686c2
Ahmed Yehia <Ahmed.Hosny4434@gmail.com>
2023-11-26 09:49:55 +0200

    suspend middelware

--- users/routes/user.js
+++ users/routes/user.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const authMiddleware = require('../../middleware/auth'); // Import authentication middleware
+// const authMiddleware = require('../../middleware/auth'); // Import authentication middleware
 
 const {
 	getAllUsers,
