de075d0feb066836866861c4028f13ebd21abbc5
Ahmed Yehia <Ahmed.Hosny4434@gmail.com>
2023-11-27 16:52:18 +0200

    set up the bot assistance

--- bot/index.js
+++ bot/index.js
@@ -3,15 +3,52 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 require("dotenv").config();
-
-console.log(process.env.OPENAI_API_KEY);
+const start = async () => {
 const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
-async function main() {
-  const completion = await openai.chat.completions.create({
-    messages: [{ role: "user", content: "but how " }],
-    model: "gpt-3.5-turbo",
-  });
 
-  console.log(completion.choices[0]);
+const assistant = await openai.beta.assistants.create({
+  "name": "DeskMate",
+  "instructions": "DeskMate is a customer service chatbot that can help you with your queries.",
+  "model" : "gpt-3.5-turbo-1106",
+});
+const thread = await openai.beta.threads.create();
+// console.log(thread.id);
+const message = await openai.beta.threads.messages.create("thread_jDCHZOaRF8fHGdI63FAhvkes", {
+  role: "user",
+  content: "are you sure ? ",
+});
+const run = await openai.beta.threads.runs.create("thread_jDCHZOaRF8fHGdI63FAhvkes", {
+  assistant_id: assistant.id,
+  instructions: "Please address the user as Mervin Praison.",    
+});
+const checkStatusAndPrintMessages = async (threadId, runId) => {
+  let runStatus = await openai.beta.threads.runs.retrieve(threadId, runId);
+  if(runStatus.status === "completed"){
+      let messages = await openai.beta.threads.messages.list(threadId);
+      messages.data.forEach((msg) => {
+          const role = msg.role;
+          const content = msg.content[0].text.value; 
+          console.log(
+              `${role.charAt(0).toUpperCase() + role.slice(1)}: ${content}`
+          );
+      });
+  } else {
+      console.log("Run is not completed yet.");
+  }  
+};
+
+setInterval(() => {
+  checkStatusAndPrintMessages("thread_jDCHZOaRF8fHGdI63FAhvkes", run.id)
+}, 7000);
 }
-main();
+start()
+
+// async function main() {
+//   const completion = await openai.chat.completions.create({
+//     messages: [{ role: "user", content: "hello " }],
+//     model: "gpt-3.5-turbo",
+//   });
+
+//   console.log(completion.choices[0].message.content);
+// }
+// main();
