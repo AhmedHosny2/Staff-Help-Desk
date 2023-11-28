e1d4974feff9017ed1f8d19402aeb01a1dbc32ce
Ahmed Yehia <Ahmed.Hosny4434@gmail.com>
2023-11-28 18:22:06 +0200

    backup at mid night

--- BackUp/backup.js
+++ BackUp/backup.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const { spawn } = require("child_process");
 const path = require("path");
+const schedule = require("node-schedule");
+
 const password = process.env.DB_PASSWORD;
 
 // Specify the MongoDB URI
@@ -15,37 +17,41 @@ const args = [
   `--uri=${mongoUri}`,
   "--gzip", // Add the --gzip option separately
 ];
-const currentDate=   (new Date().getMonth()+1) +
-    `-` +
-    new Date().getDate() +
-    `-` +
-    new Date().getFullYear()
+const currentDate =
+  new Date().getMonth() +
+  1 +
+  `-` +
+  new Date().getDate() +
+  `-` +
+  new Date().getFullYear();
 console.log(currentDate);
 
 // Set the output directory to the current directory
-const outputDirectory = path.join(
-  currentDirectory,
-  "BackUpAt" +
-  currentDate
-);
+const outputDirectory = path.join(currentDirectory, "BackUpAt" + currentDate);
 args.push(`--out=${outputDirectory}`);
 
 // Spawn the mongodump process
-const mongodumpProcess = spawn(mongodumpCommand, args);
+schedule.scheduleJob("0 0 * * *", () => {
+  const mongodumpProcess = spawn(mongodumpCommand, args);
 
-// Handle events from the mongodump process
-mongodumpProcess.stdout.on("data", (data) => {
-  console.log(`stdout: ${data}`);
+  // Handle events from the mongodump process
+  mongodumpProcess.stdout.on("data", (data) => {
+    console.log(`stdout: ${data}`);
+  });
+
+  mongodumpProcess.stderr.on("data", (data) => {
+    console.error(`stderr: ${data}`);
+  });
+
+  mongodumpProcess.on("close", (code) => {
+    console.log(`mongodump process exited with code ${code}`);
+  });
+
+  mongodumpProcess.on("error", (err) => {
+    console.error("Failed to start mongodump process:", err);
+  });
 });
 
-mongodumpProcess.stderr.on("data", (data) => {
-  console.error(`stderr: ${data}`);
-});
-
-mongodumpProcess.on("close", (code) => {
-  console.log(`mongodump process exited with code ${code}`);
-});
-
-mongodumpProcess.on("error", (err) => {
-  console.error("Failed to start mongodump process:", err);
-});
+// schedule.scheduleJob("*/2 * * * * *", function () {
+//   console.log("Running the backup script");
+// });
