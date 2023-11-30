29f317af5b8abe79307d6f58c99d571bd17c8a2e
Ahmed Yehia <Ahmed.Hosny4434@gmail.com>
2023-11-30 18:14:10 +0200

    add auth for logging

--- logging/routes/logging.js
+++ logging/routes/logging.js
@@ -5,7 +5,8 @@ const { getAllLogs,
     logError
 
 } = require("../controller/logging");
-
+const { verifyToken } = require("../utils/auth");
+router.use(verifyToken);
 router.get("/", getAllLogs);
 router.post("/log", logError);
 module.exports = router;
