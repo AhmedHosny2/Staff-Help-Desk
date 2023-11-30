0ee9b893d18d557530c5ce653c0e1769beedc448
MooTamer <mohamedtamer200200@gmail.com>
2023-11-30 18:34:28 +0200

    rate limiter (Mo)

--- users/utils/auth.js
+++ users/utils/auth.js
@@ -1,4 +1,19 @@
+const { rateLimit } = require('express-rate-limit');
 const getCookie = require('./cookies').getEntriesFromCookie;
+
+
+exports.limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, 
+	max: 3, // Initial limit
+	handler: (req, res) => {
+	  res.status(429).send('Too many requests. Please try again later.');
+	},
+	onLimitReached: (req, res, options) => {
+	  // This function is called when the initial limit is reached
+	  options.max += 1;        // Subsequent wrong login attempts will face a longer delay
+	  options.delayAfter = 1;  //the delay will be applied starting from the first exceeded attempt
+	},
+  });
 
 module.exports.verifyToken = (req, res, next) => {
 
