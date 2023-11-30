8eac0924ea7305366759689d7fd0f13164cbb02c
Ahmed Yehia <Ahmed.Hosny4434@gmail.com>
2023-11-30 17:32:10 +0200

    formating

--- middleware/index.js
+++ middleware/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
-const authMiddleware = require('./middleware/auth');
+// const authMiddleware = require('./middleware/auth');
 
 const app = express();
 const PORT = process.env.PORT || 5002;
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 // Middleware to verify tokens for private routes
-app.use(authMiddleware.verifyToken);
+// app.use(authMiddleware.verifyToken);
 
 app.listen(PORT, () => {
 	console.log(`Authentication Microservice is listening on port ${PORT}`);
