dd4eea902ab782f1538a9b6e713aa116d8f6e46a
Ahmed Yehia <Ahmed.Hosny4434@gmail.com>
2023-12-01 13:59:23 +0200

    fix a bug

--- bot/index.js
+++ bot/index.js
@@ -4,44 +4,50 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 require("dotenv").config();
 const start = async () => {
-const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+  const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
-const assistant = await openai.beta.assistants.create({
-  "name": "DeskMate",
-  "instructions": "DeskMate is a customer service chatbot that can help you with your queries.",
-  "model" : "gpt-3.5-turbo-1106",
-});
-const thread = await openai.beta.threads.create();
-// console.log(thread.id);
-const message = await openai.beta.threads.messages.create("thread_jDCHZOaRF8fHGdI63FAhvkes", {
-  role: "user",
-  content: "are you sure ? ",
-});
-const run = await openai.beta.threads.runs.create("thread_jDCHZOaRF8fHGdI63FAhvkes", {
-  assistant_id: assistant.id,
-  instructions: "Please address the user as Mervin Praison.",    
-});
-const checkStatusAndPrintMessages = async (threadId, runId) => {
-  let runStatus = await openai.beta.threads.runs.retrieve(threadId, runId);
-  if(runStatus.status === "completed"){
+  const assistant = await openai.beta.assistants.create({
+    name: "DeskMate",
+    instructions:
+      "DeskMate is a customer service chatbot that can help you with your queries.",
+    model: "gpt-3.5-turbo-1106",
+  });
+  const thread = await openai.beta.threads.create();
+  // console.log(thread.id);
+  const message = await openai.beta.threads.messages.create(
+    thread.id,
+    {
+      role: "user",
+      content: "are you sure ? ",
+    }
+  );
+  const run = await openai.beta.threads.runs.create(
+    thread.id,
+    {
+      assistant_id: assistant.id,
+    }
+  );
+  const checkStatusAndPrintMessages = async (threadId, runId) => {
+    let runStatus = await openai.beta.threads.runs.retrieve(threadId, runId);
+    if (runStatus.status === "completed") {
       let messages = await openai.beta.threads.messages.list(threadId);
       messages.data.forEach((msg) => {
-          const role = msg.role;
-          const content = msg.content[0].text.value; 
-          console.log(
-              `${role.charAt(0).toUpperCase() + role.slice(1)}: ${content}`
-          );
+        const role = msg.role;
+        const content = msg.content[0].text.value;
+        console.log(
+          `${role.charAt(0).toUpperCase() + role.slice(1)}: ${content}`
+        );
       });
-  } else {
+    } else {
       console.log("Run is not completed yet.");
-  }  
-};
+    }
+  };
 
-setInterval(() => {
-  checkStatusAndPrintMessages("thread_jDCHZOaRF8fHGdI63FAhvkes", run.id)
-}, 7000);
-}
-start()
+  setTimeout(() => {
+    checkStatusAndPrintMessages(thread.id, run.id);
+  }, 7000);  //change the time to appropriate time
+};
+start();
 
 // async function main() {
 //   const completion = await openai.chat.completions.create({
