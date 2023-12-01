32d2e142a277af991affda0614fa53a1ca9ebfef
Ahmed Yehia <Ahmed.Hosny4434@gmail.com>
2023-12-01 15:50:41 +0200

    adding auth for different roles

--- knowledgeBase/routes/KnowledgeBase.js
+++ knowledgeBase/routes/KnowledgeBase.js
@@ -8,9 +8,11 @@ const {
   modifyFaq,
   createFaq,
 } = require("../controller/add-remove-modify");
-const { verifyToken } = require("../utils/auth");
+const { verifyToken ,verifyAdminRole,verifyAgentRole,verifyManagerRole } = require("../utils/auth");
 router.use(verifyToken);
-
+// router.use(verifyAgentRole);
+// router.use(verifyAdminRole);
+// router.use(verifyManagerRole);
 router.get("/", getAllKnowledgeBase);
 router.delete("/:id", removeFaq);
 router.patch("/:id", modifyFaq);
