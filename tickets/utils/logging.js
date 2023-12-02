e0a8b1452fd94238c0853472809546822d0803cb
bahylol <94308278+bahylol@users.noreply.github.com>
2023-12-02 23:40:00 +0200

    testing

--- tickets/utils/logging.js
+++ tickets/utils/logging.js
@@ -1,10 +1,10 @@
 const axios = require('axios');
 
-exports.logError = async (req, statuscode, method, api, details, userId) => {
+exports.logError = async (req, statuscode, method, api, details, jwt) => {
     try {
         const ip = req.ip || req.connection.remoteAddress;
         const ipaddress = ip.split(':')[3]
-        const log = { statuscode, method, api, details, userId, ipaddress };
+        const log = { statuscode, method, api, details, jwt, ipaddress };
         await axios.post('http://localhost:5007/logging/log', log);
     } catch (err) {
         console.error('Log failed', err.message);
