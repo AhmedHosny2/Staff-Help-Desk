02d62e984b4e0706e42e6513f580811ddb146c07
bahylol <94308278+bahylol@users.noreply.github.com>
2023-12-04 14:32:39 +0200

    Updated routing

--- logging/routes/logging.js
+++ logging/routes/logging.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const { getAllLogs, logError } = require("../controller/logging");
+const { getLogs, getAdvancedLogs, logError } = require("../controller/logging");
 const { verifyToken, verfiyRole } = require("../utils/middleware");
+
+router.post("/log", logError);
 router.use(verifyToken);
 router.use(verfiyRole);
-router.get("/", getAllLogs);
-router.post("/log", logError);
+router.get("/", getLogs);
+router.get("/advanced", getAdvancedLogs);
 module.exports = router;
