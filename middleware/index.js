dadd1da1e7059ea180727c49534bc6595288af18
Ahmed Yehia <Ahmed.Hosny4434@gmail.com>
2023-12-07 12:05:17 +0200

    fix google auth

--- middleware/index.js
+++ middleware/index.js
@@ -1,30 +1,43 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
+const passport = require("passport");
 const passportSetup = require('./utils/passport');
 const authRoute = require('./utils/auth');
 require('dotenv').config();
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.use("/auth", authRoute);
 
 
 
 const middlewareRoute = require('./utils/auth');
 
 const app = express();
+// use session 
+const session = require('express-session');
+app.use(session({
+	secret: process.env.SESSION_SECRET,
+	resave: false,
+	saveUninitialized: false,
+	cookie: {
+		sameSite: 'none',
+		secure: true,
+		maxAge: 1000 * 60 * 60 * 24 * 7 // One week
+	}
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 //session
 app.use(cookieParser()); // Add cookie parser middleware
 
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 
 
 
 // Routes
+app.use("/auth", authRoute);
 app.use('/', middlewareRoute);
 
 // Handle unspecified routes
