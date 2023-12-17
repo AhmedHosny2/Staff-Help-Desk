ed60b9d195125a03a14dbf7bc4f2c1ba36c5e441
Youssef Elwy <102544122+youfiElwy@users.noreply.github.com>
2023-12-17 16:40:14 +0200

    updated the ML model
    Random Forest
    and new data set

--- ticketClassifierApi/ml_api.py
+++ ticketClassifierApi/ml_api.py
@@ -4,9 +4,10 @@ import pandas as pd
 
 app = Flask(__name__)
 
-# Load the trained model and the encoder
-model = load('./saved_model/model.joblib')
-encoder = load('./saved_model/encoder.joblib')
+# Load the trained model and the encoder and the agent_order
+model = load('./saved_model/model_rf.joblib')
+encoder = load('./saved_model/encoder_rf.joblib')
+agent_order = load('./saved_model/agent_order_rf.joblib')
 
 # # Read the input data
 # input_data = pd.DataFrame([['low', 'network']], columns=['Priority', 'Type'])
@@ -21,34 +22,36 @@ encoder = load('./saved_model/encoder.joblib')
 
 @app.route('/api/assignTicket', methods=['POST'])
 def assign_ticket():
-   try:
+    try:
+        # Extract ticket information from the request
+        data = request.get_json()
+        priority = data['priority']
+        ticket_type = data['type']
 
-      # Extract ticket information from the request
-      data = request.get_json()
-      priority = data['priority']
-      ticket_type = data['type']
+        # Read the input data
+        input_data = pd.DataFrame([[str(priority), str(ticket_type)]], columns=['Priority', 'Type'])
 
-      # Read the input data
-      input_data = pd.DataFrame([[str(priority), str(ticket_type)]], columns=['Priority', 'Type'])
+        # One-hot encode the input data using the loaded encoder
+        input_encoded = encoder.transform(input_data)
 
-      # One-hot encode the input data using the loaded encoder
-      input_encoded = encoder.transform(input_data)
+        # Make predictions using the loaded model
+        predicted_probabilities = model.predict_proba(input_encoded)[0]
 
-      # Make predictions using the loaded model
-      predicted_agent = model.predict(input_encoded).tolist()
+        # Sort the predicted agents by probability
+        #sorted_agents = [agent for _, agent in sorted(zip(predicted_probabilities, agent_order), reverse=True)]
+        sorted_agents = ['agent1', 'agent2', 'agent3']
 
-      print(predicted_agent[0])
 
-      # Create a response with the predicted agent
-      response = {
-         'predicted_agent': predicted_agent[0]
-      }
+        # Create a response with the predicted agents and their probabilities
+        response = {
+            'predicted_agents': [{'agent': agent, 'probability': prob} for agent, prob in zip(sorted_agents, predicted_probabilities)]
+        }
 
-      return jsonify(response), 200
+        return jsonify(response), 200
 
-   except Exception as e:
-      print(e)
-      return jsonify({'error': 'Internal Server Error'}), 500
+    except Exception as e:
+        print(e)
+        return jsonify({'error': 'Internal Server Error'}), 500
 
 if __name__ == '__main__':
     app.run(port=5012)
