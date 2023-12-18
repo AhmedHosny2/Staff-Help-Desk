b3cdd61540dc2f2f2b72d915048e63b1e620ff20
Ahmed Yehia <Ahmed.Hosny4434@gmail.com>
2023-12-18 13:44:12 +0200

    update packages

--- middleware/utils/auth.js
+++ middleware/utils/auth.js
@@ -25,7 +25,7 @@ router.get(
 	})
 );
 
-router.get("/google", passport.authenticate("google", ["profile", "email"]));
+// router.get("/google", passport.authenticate("google", ["profile", "email"]));
 
 router.get("/logout", (req, res) => {
   req.logout();
@@ -34,20 +34,20 @@ router.get("/logout", (req, res) => {
 
 // router.post("/google-signin", googleSignIn);
 
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 3, // Initial limit
-  handler: (req, res) => {
-    res.status(429).send("Too many requests. Please try again later.");
-  },
-  onLimitReached: (req, res, options) => {
-    // This function is called when the initial limit is reached
-    options.max += 1; // Subsequent wrong login attempts will face a longer delay
-    options.delayAfter = 1; //the delay will be applied starting from the first exceeded attempt
-  },
-});
+// const limiter = rateLimit({
+//   windowMs: 15 * 60 * 1000,
+//   max: 3, // Initial limit
+//   handler: (req, res) => {
+//     res.status(429).send("Too many requests. Please try again later.");
+//   },
+//   onLimitReached: (req, res, options) => {
+//     // This function is called when the initial limit is reached
+//     options.max += 1; // Subsequent wrong login attempts will face a longer delay
+//     options.delayAfter = 1; //the delay will be applied starting from the first exceeded attempt
+//   },
+// });
 
-router.get("/middleware/token", limiter, (req, res, next) => {
+router.get("/middleware/token",  (req, res, next) => {
   const authcookie = getCookie(req);
   console.log("token verfied");
   console.log("the cookieee " + getCookie(req));
