d26380477dbc445b0caa85905d2fd49478e04679
bahylol <94308278+bahylol@users.noreply.github.com>
2023-12-20 20:40:14 +0200

    Update initializeAll.py

--- initializeAll.py
+++ initializeAll.py
@@ -44,7 +44,7 @@ def write_env_file(folder_path, env_data):
 users_folder_path = 'users'
 write_env_file(users_folder_path, {
     'CLIENT_URL': CLIENT_URL,
-    'PORT': '5001',
+    'PORT': '5010',
     'CONNECTION_URL': CONNECTION_URL,
     'ACCESS_TOKEN_SECRET': ACCESS_TOKEN_SECRET,
     'REFRESH_TOKEN_SECRET': REFRESH_TOKEN_SECRET,
