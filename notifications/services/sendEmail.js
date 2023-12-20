4c8fb8a8fb76ddb3437b69c40f47e6b5129a04ea
Youssef Elwy <102544122+youfiElwy@users.noreply.github.com>
2023-12-20 17:26:52 +0200

    added agent send email route

--- notifications/services/sendEmail.js
+++ notifications/services/sendEmail.js
@@ -2,9 +2,7 @@ const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.DESKMATE_SENDGRID_API_KEY);
 const signupMailTemplate = require('./emailTemplates/signupMail.js');
 const resetPassMail = require('./emailTemplates/resetPassMail.js');
-const test = require('./emailTemplates/test.js');
-
-async function sendEmail(to, subject, text, resetLink = '') {
+async function sendEmail(to, subject, text, resetLink = '', data = null) {
 	let html = '';
 
 	if (text === 'signup') {
@@ -29,10 +27,49 @@ async function sendEmail(to, subject, text, resetLink = '') {
 			</div>`;
 
 		html = await signupMailTemplate(subject, text);
-		// html = await test();
 	} else if (text === 'reset password') {
 		text = 'Click on the link below to reset your password!';
 		html = await resetPassMail(text, resetLink);
+	} else if (text === 'ticket update') {
+		text = `
+		<div>
+		<p>
+			 Welcome to Desk Mate, your all-in-one solution for seamless task management and support!
+			 We're thrilled to have you on board. Our team is dedicated to providing you with the best user experience,
+			 and we're here to support you every step of the way.
+		</p>
+		<br /><br />
+		<p>
+			 If you ever need assistance, have questions, or encounter any technical issues, our help center is
+			 just a click away. Our knowledgeable support team is ready to assist you in making the most out of
+			 Desk Mate. Feel free to explore our comprehensive features and make your work life more organized and efficient.
+		</p>
+		<br /><br />
+		<p>
+			 Thank you for choosing Desk Mate. We're pleased to inform you that your ticket regarding the following issue has been solved:
+		</p>
+		<ul>
+			 <li><strong>Issue:</strong> ${data.issue}</li>
+			 <li><strong>Category:</strong> ${data.category}</li>
+		</ul>
+		<p>
+			<strong>Description:</strong>
+			<br /><br />
+			${data.description}
+		</p>
+		<p>
+			<strong>Solution:</strong>
+			<br /><br />
+			${data.solution}
+		</p>
+		<br /><br />
+		<p>
+			 We look forward to being part of your journey towards enhanced productivity.
+		</p>
+		<br /><br />
+  </div>`;
+
+		html = await signupMailTemplate(subject, text);
 	}
 
 	const msg = {
@@ -40,24 +77,14 @@ async function sendEmail(to, subject, text, resetLink = '') {
 		from: 'deskmatenoreply@gmail.com',
 		subject: subject,
 		html,
-		// text: 'test text',
-		// templateId: process.env.DESKMATE_POSTCARDS_TEMPLATE_KEY,
-		// dynamicTemplateData: {
-		// 	subject: 'TEST SUBJECT',
-		// 	text: 'THIS IS THE TEST TEXT BODY',
-		// },
 	};
 
 	await sgMail
 		.send(msg)
-		.then(() => {
-			// console.log('Email sent!!!');
-		})
+		.then(() => {})
 		.catch((error) => {
 			console.error(error);
 		});
 }
-
-// Export the sendEmail function
 
 module.exports = sendEmail;
