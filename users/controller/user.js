4685c17476b2742d1b2e378f87e72f09eb21f4eb
Ahmed Yehia <Ahmed.Hosny4434@gmail.com>
2023-12-22 17:20:37 +0200

    httponly false

--- users/controller/user.js
+++ users/controller/user.js
@@ -521,7 +521,7 @@ exports.loginUser = async (req, res) => {
 
     // Set the token as a cookie (optional)
     res.cookie("authcookie", token, {
-      httpOnly: true,
+      // httpOnly: true,
       secure: true,
       sameSite: "none",
       expires: new Date(Date.now() + 10 * 60 * 60 * 1000), // Expires in 1 hour
