a2ceba3bd1054aba69bc2f09c6fd7dc868a54b8c
Ahmed Osama <98207790+AhmedHosny2@users.noreply.github.com>
2023-12-25 18:01:40 +0200

    add DB restore with date

--- BackUp/backup.js
+++ BackUp/backup.js
@@ -8,7 +8,7 @@ const password = process.env.DB_PASSWORD;
 
 // Specify the MongoDB URI
 const mongoUri = `mongodb+srv://staffhelpdesk0:${password}@cluster0.p8xq0a1.mongodb.net/SEProjectDB?retryWrites=true&w=majority`;
-
+const mongoUriRestore = process.env.BACKUP_URI;
 // Get the current directory of the script
 const currentDirectory = __dirname;
 
@@ -19,9 +19,10 @@ const args = [
   "--gzip", // Add the --gzip option separately
 ];
 const currentDate =
-  (new Date().getMonth() + 1) +
+  new Date().getMonth() +
+  1 +
   "-" +
-  new Date().getDate() +   //for day 
+  new Date().getDate() + //for day
   "-" +
   new Date().getFullYear();
 
@@ -35,7 +36,8 @@ args.push(`--out=${outputDirectory}`);
 // after every 10 seconds
 // schedule.scheduleJob("*/10 * * * * *", () => {
 // at midnight
-schedule.scheduleJob('0 0 * * *', () => {    const mongodumpProcess = spawn(mongodumpCommand, args);
+schedule.scheduleJob("0 0  * * * *", () => {
+  const mongodumpProcess = spawn(mongodumpCommand, args);
 
   // Handle events from the mongodump process
   mongodumpProcess.stdout.on("data", (data) => {
@@ -56,3 +58,38 @@ schedule.scheduleJob('0 0 * * *', () => {    const mongodumpProcess = spawn(mong
     console.error("Failed to start mongodump process:", err);
   });
 });
+
+// restore
+
+function restoreBackup(backupData) {
+  console.log("restoring backup");
+  const backUpPath = path.join(currentDirectory, "BackUpAt" + backupData);
+  console.log(backUpPath);
+  console.log(mongoUriRestore);
+  const mongorestoreCommand = "mongorestore";
+  const argsRestore = [
+    `--uri=${mongoUriRestore}`,
+    `--gzip`,
+    backUpPath + "/SEProjectDB",
+  ];
+
+  const mongorestoreProcess = spawn(mongorestoreCommand, argsRestore);
+
+  mongorestoreProcess.stdout.on("data", (data) => {
+    console.log(`mongorestore stdout: ${data}`);
+  });
+
+  mongorestoreProcess.stderr.on("data", (data) => {
+    console.error(`mongorestore stderr: ${data}`);
+  });
+
+  mongorestoreProcess.on("close", (code) => {
+    console.log(`mongorestore process exited with code ${code}`);
+  });
+
+  mongorestoreProcess.on("error", (err) => {
+    console.error("Failed to start mongorestore process:", err);
+  });
+}
+// const data = "11-11-2023";
+// restoreBackup(data);
