2282cb08550c6047fef828396efbb00bc7b76053
Ahmed Osama <98207790+AhmedHosny2@users.noreply.github.com>
2023-12-26 17:36:04 +0200

    adding live notf

---
+++ liveNotification/index.js
@@ -0,0 +1,46 @@
+import { Server } from "socket.io";
+
+const io = new Server({
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+let onlineUsers = [];
+
+const addNewUser = (username, socketId) => {
+  !onlineUsers.some((user) => user.username === username) &&
+    onlineUsers.push({ username, socketId });
+};
+
+const removeUser = (socketId) => {
+  onlineUsers = onlineUsers.filter((user) => user.socketId !== socketId);
+};
+
+const getUser = (username) => {
+  return onlineUsers.find((user) => user.username === username);
+};
+
+io.on("connection", (socket) => {
+  console.log("SAllllam");
+  socket.on("newUser", (username) => {
+  
+    addNewUser(username, socket.id);
+  });
+
+  socket.on("sendNotification", ({ senderName, receiverName, type }) => {
+    const receiver = getUser(receiverName);
+    io.emit("getNotification", {
+      senderName,
+      type,
+    });
+  });
+
+
+
+  socket.on("disconnect", () => {
+    removeUser(socket.id);
+  });
+});
+
+io.listen(5011);
