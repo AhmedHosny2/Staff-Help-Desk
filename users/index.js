3580eb588bed8b50e50ce7043b3d4b76782c700c
Ahmed Osama <98207790+AhmedHosny2@users.noreply.github.com>
2023-12-26 21:44:19 +0200

    Update index.js

--- users/index.js
+++ users/index.js
@@ -21,7 +21,9 @@ app.use(cookieParser()); // Add cookie parser middleware
 app.use('/user', userRouter);
 
 app.use('/brandInfo',brandInfoRouter); //brandInfo
-
+app.get("/", (req, res) => {
+	res.send("Hello World!");
+  });
 // Handle unspecified routes
 app.all('*', (req, res) => {
 	res.status(404).json({
