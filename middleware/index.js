5a8dd7711334352016e99bf9fac44a4af7507a2f
Ahmed Osama <98207790+AhmedHosny2@users.noreply.github.com>
2024-01-13 23:39:24 +0200

    Update index.js

--- middleware/index.js
+++ middleware/index.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
-const passport = require("passport");
-const passportSetup = require('./utils/passport');
-const authRoute = require('./utils/auth');
 require('dotenv').config();
 
 
@@ -12,32 +9,18 @@ const middlewareRoute = require('./utils/auth');
 
 const app = express();
 // use session 
-const session = require('express-session');
-app.use(session({
-	secret: process.env.SESSION_SECRET,
-	resave: false,
-	saveUninitialized: false,
-	cookie: {
-		sameSite: 'none',
-		secure: true,
-		maxAge: 1000 * 60 * 60 * 24 * 7 // One week
-	}
-}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 //session
 app.use(cookieParser()); // Add cookie parser middleware
 
-app.use(passport.initialize());
-app.use(passport.session());
 
 
 
 
 
 // Routes
-app.use("/auth", authRoute);
 app.use('/', middlewareRoute);
 
 // Handle unspecified routes
