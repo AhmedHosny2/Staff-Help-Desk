ba743b208b8a22100e9015d75b6dafcbf28e9168
Ahmed Osama <98207790+AhmedHosny2@users.noreply.github.com>
2024-01-13 22:46:07 +0200

    fix email url

--- users/services/BaseURLs.js
+++ users/services/BaseURLs.js
@@ -3,4 +3,5 @@ module.exports = {
   USER_BASE_URL: process.env.USER_BASE_URL,
   MIDDLEWARE_BASE_URL:process.env.MIDDLEWARE_BASE_URL,
   LOGGING_BASE_URL: process.env.LOGGING_BASE_URL,
+  NOTIFICATION_BASE_URL: process.env.NOTIFICATION_BASE_URL,
 };
