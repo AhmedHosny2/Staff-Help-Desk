3c22cc7fff7f36b73cde9bc464e2149bc41648b7
Ahmed Osama <98207790+AhmedHosny2@users.noreply.github.com>
2024-01-13 21:24:27 +0200

    fix limiter file location

--- tickets/routes/ticket.js
+++ tickets/routes/ticket.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { limiter } = require('../../users/utils/rateLimiter');
+const { limiter } = require('../utils/rateLimiter');
 const {
 	getAlltickets,
 	assignTicketML,
